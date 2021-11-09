75cdc20274da958a2226d9c0b20e173f0bf62c52
Esteban Longo <longoesteban@hotmail.com>
2021-11-09 19:58:33 -0300

    tarea JS 04

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -193,13 +193,38 @@ function mesesDelAño(array) {
   // "Enero", "Marzo" y "Noviembre", guardarlo en nuevo array y retornarlo.
   //Si alguno de los meses no está, devolver: "No se encontraron los meses pedidos"
   // Tu código:
-}
+
+  var mesesArray = [];
+
+  for (var i = 0; i < array.lenght; i++) {
+    if (array [i] === "Enero" || array[i] === "Marzo" || array[i] === "Noviembre") {
+      mesesArray.push (array[i]);
+    }
+  }
+  if (mesesArray.includes("Enero") &&
+      mesesArray.includes("Marzo") &&
+      mesesArray.includes("Noviembre") ) {
+        return mesesArray;
+      }
+      
+      return "No se encontraron los meses pedidos";
+  }
+
 
 
 function mayorACien(array) {
   //La función recibe un array con enteros entre 0 y 200. Recorrer el array y guardar en un nuevo array sólo los
   //valores mayores a 100 (no incluye el 100). Finalmente devolver el nuevo array.
   // Tu código:
+
+  var arrayMayores = []
+
+  for (var i=0; i < array.length; i++) {
+    if ( array [i] > 100) {
+      arrayMayores.push(array[i])
+    }
+   }
+  return arrayMayores;
 }
 
 
@@ -211,6 +236,18 @@ function breakStatement(numero) {
   //devolver: "Se interrumpió la ejecución"
   //Pista: usá el statement 'break'
   // Tu código:
+
+  var sumaArray = [];
+  var suma = numero
+  
+  for (var i = 0; i < 10; i++) {
+    suma = suma + 2
+  
+  if (suma === i) break
+  sumaArray.push(suma)
+  }
+
+
 }
 
 
@@ -221,6 +258,16 @@ function continueStatement(numero) {
   //Cuando el número de iteraciones alcance el valor 5, no se suma en ese caso y se continua con la siguiente iteración
   //Pista: usá el statement 'continue'
   // Tu código:
+
+ var sumArray =[]
+ var suma = numero
+ 
+ for ( var i = 0; i < 10; i++) {
+     if (i === 5) continue
+     suma = suma + 2;
+   sumArray.push(suma)
+ }
+  return sumArray
 }
 
 
