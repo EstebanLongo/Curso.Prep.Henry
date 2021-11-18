dc6e769df38647ded48832553620d9e3054798fd
Esteban Longo <longoesteban@hotmail.com>
2021-11-18 17:43:12 -0300

    JS 02

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -35,7 +35,8 @@ function suma(x, y) {
   // "x" e "y" son números
   // Suma "x" e "y" juntos y devuelve el valor
   // Tu código:
-  return x + y;
+
+    return x + y;
 }
 
 function resta(x, y) {
@@ -166,6 +167,9 @@ function esPositivo(numero) {
   //Si el número es positivo, devolver ---> "Es positivo"
   //Si el número es negativo, devolver ---> "Es negativo"
   //Si el número es 0, devuelve false
+  if (numero === 0) {
+    return false;
+  }
   if (numero > 0) {
     return "Es positivo";
   }
